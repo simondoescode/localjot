4564e4598de4705e8a66bf25e5e2dceaa6cc96a7
Simon <simonmfricker@gmail.com>
2026-09-06 00:37:41 +0100

    tailwind

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
-const CACHE="local-transcriber-v1";
+const CACHE="localjot-v3";
 const APP=["./","./index.html","./app.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("activate",e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())
+));
 self.addEventListener("fetch",e=>{
   const u=new URL(e.request.url);
   if(u.origin===location.origin){
