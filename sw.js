c373f60b614958922e7afad2815ca3410087c1a7
Simon <simonmfricker@gmail.com>
2026-09-06 00:51:29 +0100

    changes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="localjot-v3";
+const CACHE="localjot-v5";
 const APP=["./","./index.html","./app.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(
