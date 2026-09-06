f54cfcd4d61a7cc1b8abc9dd9a95752725a274d4
Simon <simonmfricker@gmail.com>
2026-09-06 19:45:07 +0100

    github pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: '/localjot/', // <-- Add this line
   plugins: [react()],
 });
